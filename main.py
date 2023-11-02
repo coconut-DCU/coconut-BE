5fcdf3bc307dc9a4aa945ce9f3029ba9f3f0a023
MuchanKim <mutopia82@gmail.com>
2023-11-02 10:34:40 +0900

    add

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from src.routes.router import router
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
