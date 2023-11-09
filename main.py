a0c25273cdedcd6042e93cab5b34ec3747a9f0e4
MuchanKim <mutopia82@gmail.com>
2023-11-09 11:30:12 +0900

    .

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from src.routes import img, search
-
+from src.routes import img, picture
+from fastapi.logger import logger
 
 app = FastAPI()
-app.include_router(img.router, prefix="/api", tags=["picture"])
+#app.include_router(img.router, prefix="/api", tags=["picture"])
+app.include_router(picture.router, prefix="/api", tags=["picture"])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    #uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
--- src/routes/picture.py
+++ src/routes/picture.py
@@ -1,18 +1,22 @@
-# 여러사진 받는 코드
 from fastapi import APIRouter, UploadFile, File
-import os
 from typing import List
+import os
+import uuid
 
 router = APIRouter()
 
-@router.post("/upload")
-async def upload_images(images: List[UploadFile]):
+@router.post("/api/upload1")
+async def upload_images(images: List[UploadFile] = File(...)):
     try:
         upload_folder = "images/"
         os.makedirs(upload_folder, exist_ok=True)
 
         for image in images:
-            with open(f"{upload_folder}{image.filename}", "wb") as f:
+            # 클라이언트에서 사용한 파일 이름을 서버에서도 동일하게 사용
+            file_name, file_extension = os.path.splitext(image.filename)
+            unique_filename = f"{file_name}_{str(uuid.uuid4())}{file_extension}"
+            
+            with open(f"{upload_folder}{unique_filename}", "wb") as f:
                 f.write(image.file.read())
 
         return {"message": f"{len(images)} images uploaded successfully"}
