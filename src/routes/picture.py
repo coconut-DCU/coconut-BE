4ac9cae7fd5146f0be11ce8cd6a3e26a991f5212
MuchanKim <mutopia82@gmail.com>
2023-11-21 23:19:25 +0900

    update picture.py - Set received image name

--- src/routes/picture.py
+++ src/routes/picture.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, UploadFile, File
 from typing import List
 import os
-import uuid
 
 router = APIRouter()
 
@@ -11,12 +10,13 @@ async def upload_images(images: List[UploadFile] = File(...)):
         upload_folder = "images/"
         os.makedirs(upload_folder, exist_ok=True)
 
-        for image in images:
-            # 클라이언트에서 사용한 파일 이름을 서버에서도 동일하게 사용
-            file_name, file_extension = os.path.splitext(image.filename)
-            unique_filename = f"{file_name}_{str(uuid.uuid4())}{file_extension}"
+        existing_files = len([f for f in os.listdir(upload_folder) if f.startswith("image_")])
+
+        for i, image in enumerate(images, 1):
+            _, file_extension = os.path.splitext(image.filename)
+            unique_filename = f"image_{existing_files + i}{file_extension}"
             
-            with open(f"{upload_folder}{unique_filename}", "wb") as f:
+            with open(os.path.join(upload_folder, unique_filename), "wb") as f:
                 f.write(image.file.read())
 
         return {"message": f"{len(images)} images uploaded successfully"}
