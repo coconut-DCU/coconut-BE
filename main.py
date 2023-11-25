701af8f58dc34625727cbb4fdd5f6900e3eaa4ee
MuchanKim <mutopia82@gmail.com>
2023-11-25 22:53:40 +0900

    change name: picture.py -> image.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
-from src.api import picture
+from src.api import image
 
 from src.settings.config import AppConfig
 
 config = AppConfig()
 app = FastAPI()
 
-app.include_router(picture.router, prefix="/api", tags=["picture"])
+app.include_router(image.router, prefix="/api", tags=["picture"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host=config.host, port=config.port)
---
+++ src/api/__init__.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+from src.api import image
+
+router = APIRouter()
+router.include_router(image.router, tag=["image"])
