2ce5f902ea8ae66761ac89defa38c79bbe225b4a
MuchanKim <mutopia82@gmail.com>
2023-11-25 21:46:14 +0900

    module update

--- src/routes/picture.py
+++ src/routes/picture.py
@@ -1,27 +1,15 @@
-import os
-
 from fastapi import APIRouter, UploadFile, File
 from typing import List
 
-from ulits import image_module
+from ..utils.image_module import *
 
 router = APIRouter()
 
 @router.post("/upload")
 async def upload_images(images: List[UploadFile] = File(...)):
+    create_upload_dir()
     try:
-        upload_folder = "images/"
-        os.makedirs(upload_folder, exist_ok=True)
-
-        existing_files = len([f for f in os.listdir(upload_folder) if f.startswith("image_")])
-
-        for i, image in enumerate(images, 1):
-            _, file_extension = os.path.splitext(image.filename)
-            unique_filename = f"image_{existing_files + i}{file_extension}"
-            
-            with open(os.path.join(upload_folder, unique_filename), "wb") as f:
-                f.write(image.file.read())
-
+        save_uploded_images(images)
         return {"message": f"{len(images)} images uploaded successfully"}
     except Exception as e:
         return {"error": f"Failed to upload images: {e}"}
--- src/utils/image_module.py
+++ src/utils/image_module.py
@@ -1,4 +1,16 @@
 import os
 
-def create_upload_forder(forder_name):
-    os.makedirs(forder_name, exist_ok=True)
+def create_upload_dir(dir_name="images/"):
+    os.makedirs(dir_name, exist_ok=True)
+    
+def save_uploded_images(images="images", dir_name="images/"):
+    existing_files = len([f for f in os.listdir(dir_name) if f.startswith("image_")])
+    
+    for i, image in enumerate(images, 1):
+            _, file_extension = os.path.splitext(image.filename)
+            unique_filename = f"image_{existing_files + i}{file_extension}"
+            
+            print(unique_filename)
+            
+            with open(os.path.join(dir_name, unique_filename), "wb") as f:
+                f.write(image.file.read())
