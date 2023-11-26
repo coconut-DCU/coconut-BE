483ff1d559c889f76732c6e02b142f82ee97d9d2
KrJHS <38933033+KrJHS@users.noreply.github.com>
2023-11-26 22:12:21 +0900

    get_common_tags update

--- src/services/image_to_song.py
+++ src/services/image_to_song.py
@@ -6,12 +6,25 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from settings import path
 
+def get_common_tags(tags_list):
+    #입력된 모든 태그 리스트에 대해 or 연산을 수행합니다.
+    common_tags = tags_list[0]
+    for tags in tags_list[1:]:
+        common_tags = [a or b for a, b in zip(common_tags, tags)] 
+    return common_tags
+
 def get_recommend_songs():
     # 이미지 프로세서 인스턴스를 생성합니다.
     image_processor = ImageProcessor(model_path=path.MODEL_PATH)
+    
+    # 이미지의 경로를 정의합니다. 여러개 가능
+    image_paths = [path.IMG_PATH+"/image_1.jpg", path.IMG_PATH+"/image_2.jpg"]
 
-    # 이미지에서 태그를 추출합니다.
-    query_tags = image_processor.extract_tags(image_path=path.IMG_PATH+"/image_3.jpg")
+    # 각 이미지의 태그를 추출합니다.
+    extracted_tags = [image_processor.extract_tags(image_path=image_path) for image_path in image_paths]
+
+    # 입력된 모든 이미지의 태그에 대해 공통 태그를 찾습니다.
+    common_query_tags = get_common_tags(extracted_tags)
 
     # 유사성 검색 인스턴스를 생성합니다.
     similarity_search = SimilaritySearch(tag_table_path=path.TAG_TABLE_PATH)
@@ -20,7 +33,7 @@ def get_recommend_songs():
     similarity_search.build_index()
 
     # 유사한 플레이리스트의 playlist_songs_id를 검색합니다.
-    similar_playlists_songs_ids = similarity_search.search_similar_playlists(query_tags=query_tags)
+    similar_playlists_songs_ids = similarity_search.search_similar_playlists(query_tags=common_query_tags)
 
     # 각 playlist_songs_ids에 대해 get_top_songs을 호출하고 결과를 합칩니다.
     all_top_songs = pd.DataFrame()
@@ -38,4 +51,5 @@ def get_recommend_songs():
     all_top_songs = all_top_songs.sort_values(by='popularity', ascending=False).head(5)
 
     # 추천된 곡 정보를 출력합니다.
+    print(common_query_tags)
     print(all_top_songs)
