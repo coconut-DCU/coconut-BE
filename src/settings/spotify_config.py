741f317f4d844fc9f15aec7f38e01e2400149205
MuchanKim <mutopia82@gmail.com>
2024-10-11 22:31:18 +0900

    Update spotify_config.py

--- src/settings/spotify_config.py
+++ src/settings/spotify_config.py
@@ -1,3 +1,7 @@
 # Spotify Keys
-CLIENT_ID = '9519c3a0242b447db512032b93286813'
-CLIENT_KEY = '1a9082f6fd564a948a0a32b6685d097a'
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+CLIENT_ID = config['SPOTIFY']['CLIENT_ID']
+CLIENT_KEY = config['SPOTIFY']['CLIENT_KEY']
